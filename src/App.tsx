4b5cd2a6a4d3220504feccb8d42b3d84bd169727
aaakash06 <akashbagale182@gmail.com>
2024-12-18 21:10:21 +0545

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { ReactReader } from "react-reader";
 
-export const App = () => {
+export default function App() {
   const [location, setLocation] = useState<string | number>(0);
   return (
     <div style={{ height: "100vh" }}>
@@ -12,4 +12,4 @@ export const App = () => {
       />
     </div>
   );
-};
+}
